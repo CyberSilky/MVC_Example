9ef2c0454adadb760d2bae6caeb00d80a343efa7
James Shoemaker <james.shoemaker99@gmail.com>
2020-10-27 13:46:43 -0500

    Add project files.

---
+++ MVC_Example/Data/MVC_ExampleContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC_Example.Models
+{
+    public class MVC_ExampleContext : DbContext
+    {
+        public MVC_ExampleContext (DbContextOptions<MVC_ExampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_Example.Models.Product> Product { get; set; }
+    }
+}
---
+++ MVC_Example/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVC_Example.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public double ProductPrice { get; set; }
+        public string ProductCategory { get; set; }
+
+    }
+}
